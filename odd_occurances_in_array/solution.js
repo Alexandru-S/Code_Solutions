83b4bdbc53b2927152cc2ea70b06ac0d24ffc50c
Alex <alex.sulea@gmail.com>
2020-06-26 00:25:35 +0100

    added odd occurance in js , not done

---
+++ odd_occurances_in_array/solution.js
@@ -0,0 +1,14 @@
+
+function solution(A) {
+    var N = A
+    N.sort(function(a,b){return b-a})
+    for(var i = 0 ; i < N.length; i++){
+        if(N[i+1] == undefined){
+            return parseInt(N.join(''))
+        }
+        if(N[i] === N[i+1]){
+            N.splice(i,2)
+            i=-1
+        }
+    }
+}
