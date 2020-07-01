4949d129273d2ae9f3970414c3f9456985fc3e17
Alex <alex.sulea@gmail.com>
2020-07-02 00:23:52 +0100

    added python odd array occcurance solution

---
+++ odd_occurances_in_array/solution.py
@@ -0,0 +1,12 @@
+def solution(A):
+    A.sort()
+    for i in reversed(range(len(A))):
+        if(len(A) == 1):
+            break
+        if(i == len(A)):
+            continue
+        if(A[i] == A[i-1]):
+            A.pop(i)
+            A.pop(i-1)
+            i = i -2
+    return A[0]
