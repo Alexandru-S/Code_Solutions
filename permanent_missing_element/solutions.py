2b568b74e7fc6f5aa925b2fb409e27ca6984e908
Alex <alex.sulea@gmail.com>
2020-07-03 00:25:30 +0100

    perm missing wlement 70 perent python

---
+++ permanent_missing_element/solutions.py
@@ -0,0 +1,12 @@
+def solution(A):
+    if len(A) == 0:
+        return 1
+    A.sort()
+    if A[0] != 1
+        return 1
+    if len(A) == 1 and A[0] == 1:
+        return 2
+    for i in A:
+        if A[i]+1 != A[i+1]:
+            return A[i]+1
+    return A[-1] +1
