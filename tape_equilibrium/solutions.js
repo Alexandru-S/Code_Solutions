1940632fea5622450620060a01afe68c8d41d74d
Alex <alex.sulea@gmail.com>
2020-07-04 23:29:22 +0100

    added tape equilibrium , need to check

--- tape_equilibrium/solutions.js
+++ tape_equilibrium/solutions.js
@@ -1,4 +1,23 @@
-function solution(A) {
-    var sum_array = A.reduce(function(a, b){ return a + b})
+function solution(A){
+    // write your code in Javascript ecma 6
+
+    var sum1 = A[0];
+    var sum2 = 0;
+    var P = 1;
+    for (var i = P; i < A.length; i++) {
+        sum2 += A[i];
+    }
+    var diff = Math.abs(sum1 - sum2);
+
+    for (; P < A.length-1; P++) {
+        sum1 += A[P];
+        sum2 -= A[P];
+
+        var newDiff = Math.abs(sum1 - sum2);
+        if (newDiff < diff) {
+            diff = newDiff;
+        }
+    }
+    return diff;
 
 }
