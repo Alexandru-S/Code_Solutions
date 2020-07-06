91ccddb90ca84b9f9194d5eeafcf434966c90b5d
Alex <alex.sulea@gmail.com>
2020-07-06 23:53:58 +0100

    max counetrs , need to redo

---
+++ max_cns/slns.js
@@ -0,0 +1,30 @@
+function solution(N, A){
+
+    var lastMax = 0;
+    var max = 0;
+    var counters =  new Array (N).fill(0)
+
+    for(var j=0; j < A.length; j++){
+        if(A[j] < (N + 1)){
+            var i = A[j] - 1;
+            if (counters[i] < lastMax){
+                counters[i] = lastMax;
+            }
+            counters[i]++;
+            if (max < counters[i]){
+                max = counters[i];
+            }
+        } else {
+            lastMax = max;
+        }
+    }
+
+    for(var j = 0; j < N; j++){
+      if (counters[j] < lastMax){
+        counters[j] = lastMax;
+      }
+    }
+
+    return counters;
+
+}
