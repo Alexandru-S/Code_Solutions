48228cca705ae1de3e07d76ebd7f2c5554f16192
alex sulea <alex.sulea@gmail.com>
2020-08-08 23:07:50 +0100

    small changes

--- AbsDistinct/binary_gap.js
+++ AbsDistinct/binary_gap.js
@@ -1,6 +1,6 @@
 
 console.log("solution", solution([-5,-3,-1,0,3,6]))
-
+//TODO add code, rename folder
 function solution(A){
     return [...new Set(A.map(s => Math.abs(s)))].length;
 }
---
+++ tape_equilibrium/solutions.py
@@ -0,0 +1,13 @@
+A = [3,1,2,4,3]
+
+
+def solution(A):
+    def solution(A):
+        s = sum(A)
+        m = float('inf')
+        left_sum = 0
+        for x in A[:-1]:
+            left_sum += x
+            m = min(abs(s - 2 * left_sum), m)
+
+        return m
