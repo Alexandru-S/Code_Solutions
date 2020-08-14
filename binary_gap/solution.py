1d2b5dd316222fb04133da5464ea8ebd619d0816
alex sulea <alex.sulea@gmail.com>
2020-08-14 23:36:56 +0100

    improved binary sort

--- binary_gap/solution.py
+++ binary_gap/solution.py
@@ -1,14 +1,14 @@
 def solution(N):
     start = 0
     count = 0
-    binary = list(map(int, bin(N).replace("0b", "")))
+    binary = format(int(N), '08b')
     tracking = []
     for x in binary:
-        if start == 1 and x == 0:
+        if start == 1 and x == '0':
             count += 1
-        if start == 0 and x == 1:
+        if start == 0 and x == '1':
             start = 1
-        if start == 1 and x == 1:
+        if start == 1 and x == '1':
             tracking.append(count)
             count = 0
 
