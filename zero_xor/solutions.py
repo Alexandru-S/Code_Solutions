49c0b2d2d604335ebd0f57428755cd211e42ea41
alex sulea <alex.sulea@gmail.com>
2020-08-16 18:45:00 +0100

    added better version

--- zero_xor/solutions.py
+++ zero_xor/solutions.py
@@ -1,21 +1,14 @@
-import itertools
-
-
 N = int(input())
 arr = list(map(int, input().split(' ')))
 
+temp_val = None
 xor_count = 0
-global_count = 0
-
-
 for i in range(N):
-    if global_count == 0:
-        global_count = arr[i]
+    if not temp_val:
+        temp_val = arr[i]
     else:
-        result = arr[i] ^ global_count
+        result = temp_val ^ arr[i]
         if result == 0:
-            xor_count += 1
-        global_count = result
-
-
+            xor_count +=1
+        temp_val = result
 print(xor_count)
