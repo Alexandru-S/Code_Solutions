9a73317c7349cde46901f83c1ac5466187623148
alex sulea <alex.sulea@gmail.com>
2020-08-17 18:29:58 +0100

    added second possible solution

--- special_numbers/solutions.py
+++ special_numbers/solutions.py
@@ -27,3 +27,32 @@ for _ in range(T):
 
     out_ = Solve(k)
     print (out_)
+#####################SECOND WAY###############################################
+
+from itertools import chain, combinations
+from functools import reduce
+
+def all_subsets(arr):
+    return chain(*map(lambda x: combinations(arr,x),range(len(arr) +1)))
+
+def solve (arr):
+    all_comb = []
+
+    for x in all_subsets(arr):
+        if len(x) <= 1:
+            continue
+        if len(x) > 1:
+            all_comb.append(list(x))
+    sum_arr = []
+    for substr in all_comb:
+        res = reduce(lambda x, y: x|y, substr )
+        sum_arr.append(res)
+    return sum(sum_arr)%1000000007
+
+n = int(input())
+if n ==1 or n == 0:
+    print(0)
+else:
+    arr = list(map(int, input().split(' ')))
+    out_ = solve(arr)
+    print (out_)
