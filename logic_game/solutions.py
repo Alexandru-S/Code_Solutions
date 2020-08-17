44da28d6140f453a030010c6dcdcec8a4aa44820
alex sulea <alex.sulea@gmail.com>
2020-08-17 18:38:35 +0100

    added logic game

---
+++ logic_game/solutions.py
@@ -0,0 +1,14 @@
+#100% works
+for _ in range(int(input())):
+    n=int(input())
+    num=0
+    val=0
+    while n!=1:
+        if n%2==1:
+            val+=(2**(num+1))
+        n=n//2
+        num+=1
+    if val!=0:
+        print(val)
+    else:
+        print(pow(2,num))     
