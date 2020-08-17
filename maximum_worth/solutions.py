a58cdc227510626b01536ed6793f16937f03b7ac
alex sulea <alex.sulea@gmail.com>
2020-08-17 12:46:03 +0100

    max worth more

--- maximum_worth/solutions.py
+++ maximum_worth/solutions.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 N = int(input())
 limit = list(map(int, input().split(' ')))
 shop_matrix = []
@@ -7,10 +6,20 @@ shop_matrix = []
 for _ in range(N):
     shop_matrix.append(list(map(int, input().split(' '))))
 
-
 array1 = np.array(shop_matrix)
 array_trans = array1.transpose()
 
-shop_matrix = array_trans.tolist() 
-new_shop = [ for x in ]
-print(new_shop)
+shop_matrix = array_trans.tolist()
+sorted_matrix = []
+
+for x in shop_matrix:
+    new_arr = sorted(x, reverse=True)
+    sorted_matrix.append(new_arr)
+
+print('=-===', sorted_matrix)
+
+priced_matrix = []
+for x in range(N):
+    print('highest price', )
+    highest = sorted_matrix.index(max(sorted_matrix))
+    price =
