df46e1fdcfd6deae43ef06f0aa38f33adbf2f91c
alex sulea <alex.sulea@gmail.com>
2020-08-17 16:37:49 +0100

    added diff sum

---
+++ freq_diff_sum/solutions.py
@@ -0,0 +1,17 @@
+import collections
+
+
+mainstring = str(input())
+
+all_substrings = [ mainstring[i:j] for i in range(len(mainstring)) for j in range(i + 1, len(mainstring) + 1) if len(mainstring[i:j]) > 2]
+
+main_diff = []
+
+for x in all_substrings:
+    col1 = collections.Counter(x).most_common()
+    most_common = col1[0][1]
+    least_common = col1[-1][1]
+    main_diff.append(most_common - least_common)
+
+
+print(sum(main_diff))
