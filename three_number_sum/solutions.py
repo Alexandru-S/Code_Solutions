2ea70e7d27ae65ba57ed40a75bba86f3aaa85ed5
alex sulea <alex.sulea@gmail.com>
2020-08-20 21:54:33 +0100

    added tree number sum

---
+++ three_number_sum/solutions.py
@@ -0,0 +1,39 @@
+from itertools import combinations
+
+def getCombinations(arr, n):
+	return list(combinations(arr,n))
+
+def setLowToHigh(target_result):
+	max = len(target_result) -1
+	count = 0
+	for x in range(len(target_result)):	
+		if x != max:
+			if target_result[x][0] == target_result[x+1][0]:
+				if target_result[x][1] > target_result[x+1][1]:
+					target_result[x] , target_result[x+1] = target_result[x+1] , target_result[x]
+					count +=1
+					
+			if target_result[x][0] > target_result[x+1][0]:
+				target_result[x] , target_result[x+1] = target_result[x+1] , target_result[x]
+				count +=1
+
+	if count == 0:
+		return True
+	else:
+		return False
+		
+	
+	
+
+def threeNumberSum(array, targetSum):
+	comb_result = getCombinations(array, 3)
+	target_result = []
+	for x in comb_result:
+		lst = sorted(list(x))
+		if sum(lst) == targetSum:
+			target_result.append(lst)
+	fixed_array = False
+	while fixed_array == False:
+		fixed_array = setLowToHigh(target_result)
+    return target_result
+
