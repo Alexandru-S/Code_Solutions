c023f459ebfee54e5c4c0200c734340474a5df12
alex sulea <alex.sulea@gmail.com>
2020-08-20 15:31:56 +0100

    added validate sequnece

---
+++ validate_sequnece/solutions.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def getListofCombinations(arr, n):
+	return list(combinations(arr, n))
+
+def isValidSubsequence(array, sequence):
+	subsequences = getListofCombinations(array, len(sequence))
+	tracking_list = []
+	found = False
+	for x in subsequences:
+		if list(x) == sequence:
+			found = True
+	return found
+
