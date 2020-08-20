dbf3a1489563470cc5d27cf38d339cf2fcf34a1b
alex sulea <alex.sulea@gmail.com>
2020-08-20 15:35:28 +0100

    fixed issues two numbers

--- two_number_sum/solutions.py
+++ two_number_sum/solutions.py
@@ -1,3 +1,5 @@
+
+
 def twoNumberSum(array, targetSum):
 	tracking_array = []
     for i in array:
@@ -6,9 +8,6 @@ def twoNumberSum(array, targetSum):
 				tracking_array.append([i, j])
 		
 	for x in tracking_array:
-		x.sort()	
-	result = [ list(t) for t in set(tuple(x) for x in tracking_array)]	
-	
-			
-	result = result[0] if len(result) >=1 else result	
-	return result
+		x.sort()
+	result = [ list(t) for t in set(tuple(x) for x in tracking_array)]
+	return result[0] if len(result) >=1 else result
