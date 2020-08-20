387b79207647ba19106e9f7a2e7fc8cf15fb2efa
alex sulea <alex.sulea@gmail.com>
2020-08-20 23:32:55 +0100

    fixed binary tracking

--- binary_gap/solution.js
+++ binary_gap/solution.js
@@ -1,24 +1,24 @@
 function solution(N) {
-    var binary = Array.from(N.toString(2), Number);
-    var start = 0
-    var count = 0
-    var tracking = []
+    var binary_arr = Array.from(N.toString(2), Number);
+    var start_pos = 0;
+    var count = 0;
+    var tracking = [];
 
-    for(var i = 0 ; i < binary.length; i++){
-        if(binary[i] === 1 && start === 1){
-            tracking.push(count)
-            count = 0
+    for(var i = 0 ; i < binary_arr.length; i++){
+        if(binary_arr[i] === 1 && start_pos === 1){
+            tracking.push(count);
+            count = 0;
         }
-        else if(binary[i] === 1 && start === 0){
-            start = 1;
+        else if(binary_arr[i] === 1 && start_pos === 0){
+            start_pos = 1;
         }
-        else if( binary[i] ===  0 && start === 1){
-            count++
+        else if( binary_arr[i] ===  0 && start_pos === 1){
+            count++;
         }
     }
     tracking.sort()
     if(tracking.length){
-        return tracking[tracking.length -1]
+        return tracking[tracking.length -1];
     }else{
         return 0;
     }
