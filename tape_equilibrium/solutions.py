c9342bada3787c9d7b2178d35364fbfce8321d58
alex sulea <alex.sulea@gmail.com>
2020-08-20 23:49:16 +0100

    tape equilibrium fixed

--- tape_equilibrium/solutions.py
+++ tape_equilibrium/solutions.py
@@ -1,13 +1,11 @@
 A = [3,1,2,4,3]
 
-
 def solution(A):
-    def solution(A):
-        s = sum(A)
-        m = float('inf')
-        left_sum = 0
-        for x in A[:-1]:
-            left_sum += x
-            m = min(abs(s - 2 * left_sum), m)
+    s = sum(A)
+    m = float('inf')
+    left_sum = 0
+    for x in A[:-1]:
+        left_sum += x
+        m = min(abs(s - 2 * left_sum), m)
 
-        return m
+    return m
