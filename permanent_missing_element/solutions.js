11c980f48bbba8d96fb12dbc779cf7b341066261
alex sulea <alex.sulea@gmail.com>
2020-08-20 23:52:34 +0100

    local refactor

--- permanent_missing_element/solutions.js
+++ permanent_missing_element/solutions.js
@@ -1,15 +1,15 @@
 function solution(A) {
     if(!A.length){
-        return 1
+        return 1;
     }
     A.sort(function(a,b){return a-b})
     if(A[0] !== 1){
-        return 1
+        return 1;
     }
     for(var i = 0; i < A.length; i++){
         if(A[i]+1 !== A[i+1]){
-            return A[i] + 1
+            return A[i] + 1;
         }
     }
-    return A[A.length-1] +1
+    return A[A.length-1] +1;
 }
