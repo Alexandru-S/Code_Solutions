8b450ab975f0931c6ed0ee537213036ec7857fdf
alex sulea <alex.sulea@gmail.com>
2020-08-20 23:28:41 +0100

    fixed frog river one

--- frog_river_one/solutions.js
+++ frog_river_one/solutions.js
@@ -1,13 +1,13 @@
-
-console.log("solution", solution(5, [1,3,1,4,2,3, 5,4]))
-
 function solution(X,A){
   if (A.length === 1) { // If the array is one element
     // And if its first item is 1 as well as the value to search for,
     // the frog doesn't need to move
-    if (A[0] === 1 && X === 1) return 0;
+    if (A[0] === 1 && X === 1){
+        return 0;
+    }else{
+        return -1;
+    }
     // If not, it's impossible to go anywhere
-    else return -1;
   }
   var i = -1, // Counter
     sum = 0, // Comparison sum
@@ -17,7 +17,9 @@ function solution(X,A){
   do { // Start searching
     i++; // Increase the counter
     // If we've already found the element, continue
-    if (f[A[i]]) continue;
+    if (f[A[i]]){
+        continue;
+    }
     // If we haven't found the element, mark it
     f[A[i]] = true;
     // Add to the comparison sum that we will be using
@@ -30,9 +32,10 @@ function solution(X,A){
 
   // If we reached this point and this conditional is true,
   // we didn't find the number.
-  if (i === A.length) return -1;
+  if (i === A.length){
+    return -1;
+  }
 
   return i; // Return how long it took
-
-
 }
+console.log("solution", solution(5, [1,3,1,4,2,3, 5,4]))
