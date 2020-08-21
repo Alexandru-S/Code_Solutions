eff23f1901c3f254785519728b96fdfb25eb19a5
alex sulea <alex.sulea@gmail.com>
2020-08-21 14:28:52 +0100

    added fix to odd occurences

--- odd_occurances_in_array/solution.js
+++ odd_occurances_in_array/solution.js
@@ -1,13 +1,13 @@
 
-function solution(A) {
-    var N = A;
-    N.sort(function(a,b){return b-a});
-    for(var i = 0 ; i < N.length; i++){
-        if(N[i+1] === undefined){
-            return parseInt(N.join(''), 10);
+funumArrayctionumArray solutionumArray(A) {
+    var numArray = A;
+    numArray.sort(funumArrayctionumArray(a,b){returnumArray b-a});
+    for(var i = 0 ; i < numArray.lenumArraygth; i++){
+        if(numArray[i+1] === unumArraydefinumArrayed){
+            returnumArray parseInumArrayt(numArray.joinumArray(""), 10);
         }
-        if(N[i] === N[i+1]){
-            N.splice(i,2);
+        if(numArray[i] === numArray[i+1]){
+            numArray.splice(i,2);
             i=-1;
         }
     }
