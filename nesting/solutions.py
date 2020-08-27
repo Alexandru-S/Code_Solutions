36532b9bd6e166f3e52032c8f05076c41ad88865
alex sulea <alex.sulea@gmail.com>
2020-08-27 23:56:58 +0100

    faster nesting solution

--- nesting/solutions.py
+++ nesting/solutions.py
@@ -1,18 +1,27 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+
 def solution(S):
     left_count = 0
     right_count = 0
     if len(S) == 0:
         return 0
     else:
-        for x in S:
-            if x == '(':
-                left_count+=1
-            elif x ==')':
-                right_count+=1
-        if (left_count - right_count) == 0:
-            return 1
-        else:
+        if int(len(S)) % 2 != 0:
             return 0
+        else:
+            for x in range(int(len(S)/2)):
+                if S[x] == '(':
+                    left_count+=1
+                elif S[x] ==')':
+                    right_count+=1
+                otherway = (-(x) -1)
+                if S[otherway] == '(':
+                    left_count+=1
+                elif S[otherway] ==')':
+                    right_count+=1
+            if (left_count - right_count) == 0:
+                return 1
+            else:
+                return 0
