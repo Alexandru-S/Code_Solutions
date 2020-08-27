104ba456465d1e34a57e84292120fb47a067555f
alex sulea <alex.sulea@gmail.com>
2020-08-28 00:36:42 +0100

    resolved nested

--- nesting/solutions.py
+++ nesting/solutions.py
@@ -1,27 +1,19 @@
-# you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
-
-
+#100% time complexity O(n)
 def solution(S):
-    left_count = 0
-    right_count = 0
-    if len(S) == 0:
+    if int(len(S)%2) != 0:
         return 0
     else:
-        if int(len(S)) % 2 != 0:
-            return 0
-        else:
-            for x in range(int(len(S)/2)):
-                if S[x] == '(':
-                    left_count+=1
-                elif S[x] ==')':
-                    right_count+=1
-                otherway = (-(x) -1)
-                if S[otherway] == '(':
-                    left_count+=1
-                elif S[otherway] ==')':
-                    right_count+=1
-            if (left_count - right_count) == 0:
-                return 1
+        if int(len(S)) == 0 or S == '':
+            return 1
+        count = 0
+        for x in S:
+            if x == '(':
+                count+=1
             else:
-                return 0
+                if count == 0:
+                    return 0
+                count-=1
+        if count == 0:
+            return 1
+        else:
+            return 0
