50638b466943c8f6aaa633c937d6cfe32753f433
alex sulea <alex.sulea@gmail.com>
2020-08-30 00:56:43 +0100

    100% frog river one solutuion

--- frog_river_one/solutions.py
+++ frog_river_one/solutions.py
@@ -1,7 +1,9 @@
+# 100% time complexity O(n)
 def solution(X, A):
-    tupple = list(enumerate(A))
-    result = list(filter(lambda x: x[1] == X, tupple))
-    if result:
-        return result[0][0]
-    else:
-        return -1
+    steps = set([i for i in range(1, X + 1)])
+    frogy_steps = set()
+    for index, leaf in enumerate(A):
+        frogy_steps.add(leaf)
+        if frogy_steps == steps:
+            return index
+    return -1
