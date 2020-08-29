5bc41f3a563b5ab6df2441d1054e0f8c975a3edf
alex sulea <alex.sulea@gmail.com>
2020-08-29 23:56:14 +0100

    removed unneceasasary code

--- passing_cars/solutions.py
+++ passing_cars/solutions.py
@@ -1,8 +1,8 @@
 def solution(A):
     count = 0
     multiple = 0
-    for x in range(len(A)):
-        if A[x] == 0:
+    for x in A:
+        if x == 0:
             count+=1
         else:
             multiple += count
