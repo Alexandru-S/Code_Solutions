df6c4f08c045f34423c448669532e976dffb4b7e
alex sulea <alex.sulea@gmail.com>
2020-08-31 17:25:08 +0100

    triangle solution

---
+++ triangle/solutions.py
@@ -0,0 +1,10 @@
+#100% time complexity O(n)
+def solution(A):
+    if len(A) < 3:
+        return 0
+    else:
+        A.sort()
+        for x in range(len(A)-2):
+            if A[x] + A[x+1] > A[x+2]:
+                return 1
+        return 0
