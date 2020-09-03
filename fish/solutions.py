882c3d59e5e33e608d9f54c77fc4828642c21cea
alex sulea <alex.sulea@gmail.com>
2020-09-03 15:57:39 +0100

    surviving fish solution

--- fish/solutions.py
+++ fish/solutions.py
@@ -1,2 +1,36 @@
 def solution(A, B):
-    return 0
+    if len(A) == 0:
+        return 0
+    if len(A) == 1:
+        return 1
+    surviving_fish = []
+    current_fish = None
+    for index, value in enumerate(A):
+        if index == 0:
+            if index == 0:
+                current_fish = A[index]
+                surviving_fish.append(A[index])
+                current_fish = None
+            else:
+                if current_fish is None:
+                    current_fish = A[index]
+                if current_fish > A[index - 1]:
+                    A[index - 1] = 0
+                elif current_fish < A[index - 1]:
+                    A[index] = 0
+                    current_fish = None
+        else:
+            if index == len(A) - 1:
+                current_fish = A[index]
+                surviving_fish.append(A[index])
+                current_fish = None
+            else:
+                if current_fish is None:
+                    current_fish = A[index]
+                if current_fish > A[index + 1]:
+                    A[index + 1] = 0
+                elif current_fish < A[index + 1]:
+                    A[index] = 0
+                    current_fish = None
+    return len(surviving_fish)
+
