471e7a680a405c1619a4a95a5e3524b1d2264314
alex sulea <alex.sulea@gmail.com>
2020-09-09 22:12:41 +0100

    added fish fix

--- fish/solutions.py
+++ fish/solutions.py
@@ -1,37 +1,21 @@
-# TODO  test new solutoon
+# 100% time complexity O(n)
 def solution(A, B):
     if len(A) == 0:
         return 0
     if len(A) == 1:
         return 1
-    surviving_fish = []
-    current_fish = None
-    for index, value in enumerate(A):
-        if B[index] == 0:
-            if index == 0:
-                current_fish = A[index]
-                surviving_fish.append(A[index])
-                current_fish = None
+    survivals = 0
+    stack = []
+    for i in range(len(A)):
+        if B[i] == 0:
+            while len(stack) !=0:
+                if stack[-1] > A[i]:
+                    break
+                else:
+                    stack.pop()
             else:
-                if current_fish is None:
-                    current_fish = A[index]
-                if current_fish > A[index - 1]:
-                    A[index - 1] = 0
-                elif current_fish < A[index - 1]:
-                    A[index] = 0
-                    current_fish = None
+                survivals +=1
         else:
-            if index == len(A) - 1:
-                current_fish = A[index]
-                surviving_fish.append(A[index])
-                current_fish = None
-            else:
-                if current_fish is None:
-                    current_fish = A[index]
-                if current_fish > A[index + 1]:
-                    A[index + 1] = 0
-                elif current_fish < A[index + 1]:
-                    A[index] = 0
-                    current_fish = None
-    return len(surviving_fish)
-
+            stack.append(A[i])
+    survivals += len(stack)
+    return survivals
