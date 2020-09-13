aa33d37e883f3c6397a7a311d7675891f1df59d3
alex sulea <alex.sulea@gmail.com>
2020-09-13 22:40:44 +0100

    added dominator

---
+++ dominator/solutions.py
@@ -0,0 +1,13 @@
+def solution(A):
+    tracking_dict = {}
+    N = len(A)
+    for x in A:
+        tracking_dict[x] = tracking_dict[x] + 1 if x in tracking_dict else 1
+    sorted_dict = {k: v for k, v in sorted(tracking_dict.items(), key=lambda item: item[1], reverse=True)}
+    treshhold = N/2
+    denominator = list(sorted_dict.values())[0]
+    dominator = list(sorted_dict.keys())[0]
+    if denominator > treshhold:
+        return A.index(dominator)
+    else:
+        return -1
