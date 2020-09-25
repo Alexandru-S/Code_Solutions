74dc1d9cf1cb154359c7dbd6c921d426ac32ed91
alex sulea <alex.sulea@gmail.com>
2020-09-25 22:45:07 +0100

    removed print

--- profit/solutions.py
+++ profit/solutions.py
@@ -1,3 +1,4 @@
+# 77% correct time complexity O(n)
 def solution(A):
     highest = sorted(A, reverse = True)[0]
     lowest = sorted(A)[0]
@@ -14,6 +15,5 @@ def solution(A):
                 local_highest = x
                 profit = A[x]-local_lowest
                 profit_tracker.append(profit)
-    print(profit_tracker)
     result = sorted(profit_tracker, reverse= True)[0]
     return result
