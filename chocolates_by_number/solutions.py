e56d5de7fee9e0db4bec8eb0927c98360dd1dce1
Alexandru-S <alex.sulea@gmail.com>
2020-09-26 23:55:07 +0100

    chocolates needs to be redone

---
+++ chocolates_by_number/solutions.py
@@ -0,0 +1,16 @@
+# 12% time complexity O(n)
+def solution(N, M):
+    arr_list = list(range(N))
+    counter = True
+    tracking_list = []
+    list_index = 0
+    while counter:
+        if list_index > N - 1:
+            list_index = list_index - N
+        if list_index in tracking_list:
+            counter = False
+        else:
+            tracking_list.append(list_index)
+
+        list_index += M
+    return len(tracking_list)
