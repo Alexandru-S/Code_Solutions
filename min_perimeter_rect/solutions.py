34df4fb785973723e4b2e6c69e8f9b7063e031cb
Alexandru-S <alex.sulea@gmail.com>
2020-09-26 23:17:11 +0100

    added min perimeter 50% correct

---
+++ min_perimeter_rect/solutions.py
@@ -0,0 +1,16 @@
+from functools import reduce
+
+
+def get_perimeter(A, B):
+    return 2 * (A + B)
+
+
+def solution(N):
+    factor_list = list(factors(N))
+    pointer = int(len(factor_list) / 2)
+    result = get_perimeter(factor_list[pointer - 1], factor_list[-pointer])
+    return result
+
+
+def factors(n):
+    return set(reduce(list.__add__, ([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0)))
