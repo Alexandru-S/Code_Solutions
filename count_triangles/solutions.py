73240368462d68dcc2f09984b8ca6969a908dbbf
Alexandru-S <alex.sulea@gmail.com>
2020-09-28 00:34:15 +0100

    addded better solution for counting triangles

--- count_triangles/solutions.py
+++ count_triangles/solutions.py
@@ -1,16 +1,14 @@
-# 63% time complexity O(n**3)
+# 63% time complexity O(n**2)
 def solution(A):
-    # Count of triangles
+    A.sort()
     count = 0
-    n = len(A)
-
-    # The three loops select three
-    # different values from array
-    for i in range(n):
-        for j in range(i + 1, n):
-            for k in range(j + 1, n):
-                if (A[i] + A[j] > A[k] and
-                        A[i] + A[k] > A[j] and
-                        A[k] + A[j] > A[i]):
-                    count += 1
+    for x in range(len(A) - 1, 0, -1):
+        l = 0
+        r = x - 1
+        while l < r:
+            if A[l] + A[r] > A[x]:
+                count += r-l
+                r -= 1
+            else:
+                l +=1;
     return count
