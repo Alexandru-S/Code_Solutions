ca4193bca6ac11b99817c597785e1c8fd8b67972
Alexandru-S <alex.sulea@gmail.com>
2020-09-29 22:55:44 +0100

    max counters refactor

--- max_counters/solutions.py
+++ max_counters/solutions.py
@@ -1,10 +1,8 @@
 #66% time comlpexity O(n*m)
-from itertools import repeat
-
 def solution(N, A):
-    counter_list = list(repeat(0, N))
+    counter_list = [0] * N
     for x in A:
-        if x <= N:
+        if 1 <=  x <= N:
             counter_list = increase(x, counter_list)
         else:
             counter_list = max_counter(counter_list)
@@ -12,12 +10,11 @@ def solution(N, A):
 
 
 def increase(x, counter_list):
-    index_loc = x - 1
-    counter_list[index_loc] += 1
+    counter_list[x - 1] += 1
     return counter_list
 
 
 def max_counter(counter_list):
-    max_count = sorted(counter_list, reverse=True)[0]
-    result = list(repeat(max_count, len(counter_list)))
+    result = [max(counter_list)] * len(counter_list)
     return result
+
