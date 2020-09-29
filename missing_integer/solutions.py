86adc55df56efd033178df7a46ed87749629b8dd
Alexandru-S <alex.sulea@gmail.com>
2020-09-30 00:13:21 +0100

    added 100% edge case fix

--- missing_integer/solutions.py
+++ missing_integer/solutions.py
@@ -1,10 +1,15 @@
-# 88% O(n)
+# 100% time complexity O(n)
 def solution(A):
     simple_A = sorted(list(set(A)))
     if max(simple_A) < 0:
         return 1
     else:
         m = set(range(1,len(A)))
+        if len(m) == 0:
+            if A[0] == 1:
+                return 2
+            else:
+                return 1
         l = set(simple_A)
         result = min(m - l, default=None)
         if result  is None:
