1858b1f44a2f45e19fe48b9777ba4eec49a2efc2
Alexandru-S <alex.sulea@gmail.com>
2020-09-29 23:24:10 +0100

    100% solved

--- max_counters/solutions.py
+++ max_counters/solutions.py
@@ -1,20 +1,16 @@
-#66% time comlpexity O(n*m)
+#100% time comlpexity O(n+m)
 def solution(N, A):
     counter_list = [0] * N
+    max_1 = 0
+    max_2 = 0
     for x in A:
-        if 1 <=  x <= N:
-            counter_list = increase(x, counter_list)
+        if 1 <= x <= N:
+            if max_1 > counter_list[x - 1]:
+                counter_list[x - 1] = max_1
+            counter_list[x - 1] += 1
+            if max_2 < counter_list[x - 1]:
+                max_2 = counter_list[x - 1]
         else:
-            counter_list = max_counter(counter_list)
+            max_1 = max_2
+    counter_list = [max(max_1, i) for i in counter_list]
     return counter_list
-
-
-def increase(x, counter_list):
-    counter_list[x - 1] += 1
-    return counter_list
-
-
-def max_counter(counter_list):
-    result = [max(counter_list)] * len(counter_list)
-    return result
-
