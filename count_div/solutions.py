9c6281153158d34b24e7d173bc9c37cde6e561ee
Alexandru-S <alex.sulea@gmail.com>
2020-10-04 23:55:26 +0100

    new method

--- count_div/solutions.py
+++ count_div/solutions.py
@@ -1,8 +1,7 @@
 # 50% time complexity O(B -A)
 def solution(A, B, K):
     list_of_num = list(range(A, B + 1))
-    tracker = []
-    for x in list_of_num:
-        if x % K == 0:
-            tracker.append(x)
-    return len(tracker)
+    tracker = 0
+    if A % K == 0:
+        tracker += 1
+    return int((B/K)) - int((A /K)) + int(tracker)
