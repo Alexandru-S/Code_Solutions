49df843837594556175f66cde79844cce6b5fbd1
Alexandru-S <alex.sulea@gmail.com>
2020-10-06 00:13:55 +0100

    added avg two sum solution

--- min_avg_two_slice/solutions.py
+++ min_avg_two_slice/solutions.py
@@ -1,17 +1,16 @@
-min_avg_value = (A[0] + A[1]) / 2.0  # The mininal average
-min_avg_pos = 0  # The begin position of the first
-# slice with mininal average
-for index in xrange(0, len(A) - 2):
-    # Try the next 2-element slice
-    if (A[index] + A[index + 1]) / 2.0 < min_avg_value:
-        min_avg_value = (A[index] + A[index + 1]) / 2.0
-        min_avg_pos = index
-    # Try the next 3-element slice
-    if (A[index] + A[index + 1] + A[index + 2]) / 3.0 < min_avg_value:
-        min_avg_value = (A[index] + A[index + 1] + A[index + 2]) / 3.0
-        min_avg_pos = index
-# Try the last 2-element slice
-if (A[-1] + A[-2]) / 2.0 < min_avg_value:
-    min_avg_value = (A[-1] + A[-2]) / 2.0
-    min_avg_pos = len(A) - 2
-return min_avg_pos
+# 100% time complexity O(n)
+def solution(A):
+    min_avg = (A[0] + A[1]) / 2.0
+    min_pos = 0
+
+    for x in range(0, len(A) - 2):
+        if (A[x] + A[x + 1]) / 2.0 < min_avg:
+            min_avg = (A[x] + A[x + 1]) / 2.0
+            min_pos = x
+        if (A[x] + A[x + 1] + A[x + 2]) / 3.0 < min_avg:
+            min_avg = (A[x] + A[x + 1] + A[x + 2]) / 3.0
+            min_pos = x
+    if (A[-1] + A[-2]) / 2.0 < min_avg:
+        min_avg = (A[-1] + A[-2]) / 2.0
+        min_pos = len(A) - 2
+    return min_pos
