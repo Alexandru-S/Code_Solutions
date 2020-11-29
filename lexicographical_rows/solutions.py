598bd375240ce7b4f7f1065a244fc2e44240f6f8
Alexandru-S <alex.sulea@gmail.com>
2020-11-29 23:28:57 +0000

    fixed solution

--- lexicographical_rows/solutions.py
+++ lexicographical_rows/solutions.py
@@ -5,13 +5,13 @@ for _ in range(T):
     k = int(input())
     out_ = Solve(k)
     print (out_)
-
 albhabet = 'abcd'
 
-new_list = [p for p in itertools.product(albhabet, repeat=4)]
-properlist = []
-for x in new_list:
-    if x == x[::-1]:
-        properlist.append(x)
-print(properlist)
 
+def Solve():
+    new_list = [p for p in itertools.product(albhabet, repeat=4)]
+    properlist = []
+    for x in new_list:
+        if x == x[::-1]:
+            properlist.append(x)
+    print(properlist)
