e771f21ada9c554bcf5a7e5eb2c67ec26e12de91
Alexandru-S <alex.sulea@gmail.com>
2020-12-13 00:03:07 +0000

    added 100% On solution to 2 number sum

--- two_number_sum/solutions.py
+++ two_number_sum/solutions.py
@@ -1,5 +1,5 @@
 
-
+# 100% O(n^2) time complexity
 def twoNumberSum(array, targetSum):
     tracking_array = []
     for i in array:
@@ -11,3 +11,18 @@ def twoNumberSum(array, targetSum):
         x.sort()
     result = [ list(t) for t in set(tuple(x) for x in tracking_array)]
     return result[0] if len(result) >=1 else result
+
+# 100% O(n) time complexity
+def twoNumberSum(array, targetSum):
+	if len(array) == 0 or len(array) == 1:
+		return []
+	dictionary = {}
+	answer = []
+	for i in range(len(array)):
+		second_number = targetSum - array[i]
+		if (second_number in dictionary.keys()):
+			second_index = array.index(second_number)
+			if i != second_index:
+				return sorted([array[i], array[second_index]])
+		dictionary.update({array[i]:i})
+	return []
