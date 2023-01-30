7b364e1c7bb7ca047a114e14a141092c9cfe08de
Alexandru-S <Alexandru-S@users.noreply.github.com>
2023-01-30 20:26:45 +0100

    added test string

--- notes.py
+++ notes.py
@@ -33,6 +33,7 @@ def factors(n):
     )
 
 
+test_str = "heloo world and to everyone"
 # get all substrings of a string
 res = [
     test_str[i:j] for i in range(len(test_str)) for j in range(i + 1, len(test_str) + 1)
