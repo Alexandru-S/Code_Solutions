313c54a298ba422b7ad06a0f514cf2a8f31c5122
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2024-09-23 20:23:09 +0200

    feat: added finding unique integer in dup array

---
+++ lonely_integer/solutions.ts
@@ -0,0 +1,24 @@
+function lonelyinteger(a: number[]): number {
+    let n:number = a.length;
+    let swapped:boolean;
+    
+    do{
+        swapped = false;
+        for(let i = 0 ; i < n-1 ; i++){
+            if(a[i] > a[i+1]){
+                [a[i],a[i+1]] = [a[i+1],a[i]]
+                swapped=true
+            }
+        }
+        n--;
+        
+    }while(swapped)
+    console.log("swapped ? ", a)
+
+        for(let i = a.length - 1 ; i>0; i--){
+            if(a[i] === a[i-1]){
+                a.splice(i-1,2)
+            }
+        }
+    return a[0]
+}
