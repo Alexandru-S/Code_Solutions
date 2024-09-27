58c71d832355fee6bd34308b6f8f480a7a5fde3f
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2024-09-27 23:35:42 +0200

    feat: added comments

--- flipping_bits/solutions.ts
+++ flipping_bits/solutions.ts
@@ -1,5 +1,5 @@
 function flippingBits(n: number): number {
-    const unasignedBits:number = n >>> 0;
-    let flippedBits = ~unasignedBits >>> 0;  
+    const unasignedBits:number = n >>> 0; // convert to 32 unasigned bitwise
+    let flippedBits = ~unasignedBits >>> 0;  //flip with NOT ~ and convert back to number
     return flippedBits;
 }
