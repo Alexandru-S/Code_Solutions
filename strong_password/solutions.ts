e7c61ae64744843097e8b35cd80de12a1b581d4d
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2024-10-17 23:22:11 +0200

    fix: fixed misspelling

--- strong_password/solutions.ts
+++ strong_password/solutions.ts
@@ -1,9 +1,9 @@
 function minimumNumber(n: number, password: string): number {
-    const p:string = password; let falts:number = 0;
-       if(!/[!@#$%^&*()+-]/.test(p)) falts++;
-       if(!/[0-9]/.test(p)) falts++;
-       if(p === p.toUpperCase()) falts++;
-       if(p === p.toLowerCase()) falts++;
-       if(n + falts < 6) falts += 6 - (falts + n);
-       return falts 
+    const p:string = password; let faults:number = 0;
+       if(!/[!@#$%^&*()+-]/.test(p)) faults++;
+       if(!/[0-9]/.test(p)) faults++;
+       if(p === p.toUpperCase()) faults++;
+       if(p === p.toLowerCase()) faults++;
+       if(n + faults < 6) faults += 6 - (faults + n);
+       return faults 
    }
