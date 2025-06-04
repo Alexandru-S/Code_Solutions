21351d605c18d0979c3e8e0314cff34e2b08697d
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-06-04 22:32:34 +0200

    Update solutions.cs

--- xor_array/solutions.cs
+++ xor_array/solutions.cs
@@ -1,4 +1,8 @@
-     public static int sansaXor(List<int> arr)
+// important, this should be calculated with something using linear time
+// So instead of generating all subarrays, use a mathematical observation to solve it in linear time
+// Observation = find frequency using i + 1) * (n - i) if its even it cancels out if its odd XOR it
+
+ public static int sansaXor(List<int> arr)
     {
         int result = 0;
         int n = arr.Count;
