5badc7918c7fb29c4466475ad3b99fe8e8b760a2
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-06-21 23:42:49 +0200

    Update solutions.cs

--- minimum_bribes/solutions.cs
+++ minimum_bribes/solutions.cs
@@ -1,21 +1,21 @@
-    public static void minimumBribes(List<int> q)
+ public static void minimumBribes(List<int> q)
+{
+    int count = 0;
+    for (int i = 0; i < q.Count; i++)
     {
-        int count = 0;
-        int maxNumber = q.Max();
-        bool tooChaotic = false;
-        List<int> originalQ = Enumerable.Range(1,maxNumber).ToList();
-        for(int i = 0; i < originalQ.Count; i++){
-            if(originalQ[i] < q[i]){
-                if(q[i]- originalQ[i] > 2){
-                    tooChaotic = true;
-                    break;
-                }
-                count+= q[i]-originalQ[i];
+        if (q[i] - (i + 1) > 2)
+        {
+            Console.WriteLine("Too chaotic");
+            return;
+        }
+        for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+        {
+            if (q[j] > q[i])
+            {
+                count++;
             }
         }
-        if(tooChaotic){
-            Console.WriteLine("Too chaotic");
-        }else{
-            Console.WriteLine(count);
-        }
     }
+
+    Console.WriteLine(count);
+}
