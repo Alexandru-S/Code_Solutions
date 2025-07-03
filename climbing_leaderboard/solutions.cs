52b6bbf073a67ca51981590eb5a27c79066b6e17
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-07-03 22:16:13 +0000

    work so far, not resolved

--- climbing_leaderboard/solutions.cs
+++ climbing_leaderboard/solutions.cs
@@ -1,6 +1,17 @@
-    public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+ public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
     {
         List<int> result = new List<int>();
-        
+        Dictionary<int, int> rankedScores = ranked.Distinct().OrderByDescending(score => score).Select((score, index) => new { score, rank = index + 1 }).ToDictionary(x => x.score, x => x.rank);
+        for(int i = 0; i<player.Count; i++){
+            Boolean foundplace = false;
+            int position = 0;
+            while(!foundplace){
+                if(rankedScores[position] ==player[i] ){
+                    foundplace =true;
+                    result[i] = rankedScores[position];
+                }else if()
+            }
+            
+        }
         return result;
     }
