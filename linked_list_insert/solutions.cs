5302c5126ea354bc14f1ac7d019bee57c9350894
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-07-07 01:11:20 +0200

    Update solutions.cs

---
+++ linked_list_insert/solutions.cs
@@ -0,0 +1,15 @@
+    public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode llist, int data, int position)
+    {
+        SinglyLinkedListNode newdata = new SinglyLinkedListNode(data);
+        if(position == 0){
+            newdata.next = llist;
+            return newdata;
+        }
+        SinglyLinkedListNode curr = llist;
+        for(int i = 0; i< position -1 && curr != null; i++){
+            curr = curr.next;
+        }
+        newdata.next = curr.next;
+        curr.next = newdata;
+        return llist;
+    }
