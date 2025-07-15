3907f1fb21b588815d13416d0d3a67fe67db2323
johannacarolinep <johannacarolinep@gmail.com>
2025-07-15 23:50:54 +0200

    chore: Added Floyds theorem on cycle checking

--- linked_list_cycle/solutions.cs
+++ linked_list_cycle/solutions.cs
@@ -1,4 +1,17 @@
-    static bool hasCycle(SinglyLinkedListNode head) {
-
-
+// Floyd's Cycle Detection Algorythm
+static bool hasCycle(SinglyLinkedListNode head)
+{
+    SinglyLinkedListNode slow = head;
+    SinglyLinkedListNode fast = head;
+    while (fast != null && fast.next != null)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow == fast)
+        {
+            return true;
+        }
     }
+    return false;
+
+}
