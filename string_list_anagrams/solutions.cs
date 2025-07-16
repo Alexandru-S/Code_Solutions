e924d0783839155ac727de6757b5594ee0671c86
johannacarolinep <johannacarolinep@gmail.com>
2025-07-17 00:08:36 +0200

    chore: added anagram pairs solution

--- string_list_anagrams/solutions.cs
+++ string_list_anagrams/solutions.cs
@@ -1,24 +1,23 @@
-
 public static int sherlockAndAnagrams(string s)
 {
-    List<string> allSubStrings = new List<string>();
+    Dictionary<string,int> subStringCounts = new Dictionary<string, int>();
     for(int i =0; i<s.Length; i++){
         for(int j = 1; j<=s.Length-i; j++){
             string substring = s.Substring(i,j);
             string sorted = String.Concat(substring.OrderBy(c => c));
-            allSubStrings.Add(sorted);
+            if(subStringCounts.ContainsKey(sorted)){
+                subStringCounts[sorted]++;
+            }else{
+                subStringCoSunts[sorted] = 1;
+            }
         }
     }
-    allSubStrings.Sort();
-    int listLength = allSubStrings.Count;
     int count = 0;
-    for(int i = listLength-1; i>0; i--){
-            if(allSubStrings[i] == allSubStrings[i-1]){
-            allSubStrings.RemoveAt(i);
-            allSubStrings.RemoveAt(i-1);
-            listLength = allSubStrings.Count;
-            count++;
-            }
+    foreach(var kvp in subStringCounts){
+        int freq = kvp.Value;
+        if(freq > 1){
+            count += (freq*(freq-1))/2; 
+        }
     }
     return count;
 
