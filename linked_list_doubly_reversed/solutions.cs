3e9f2a89ad02890cafb55ebdf7b88c7cfd5c5213
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-07-18 00:34:50 +0200

    cchore: format code

--- linked_list_doubly_reversed/solutions.cs
+++ linked_list_doubly_reversed/solutions.cs
@@ -1,16 +1,16 @@
-    public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist)
-    {
-        DoublyLinkedListNode? curr = llist;
-        DoublyLinkedListNode? prev = null;
-        
-        while(curr != null){
-            prev = curr.prev;
-            curr.prev = curr.next;
-            curr.next = prev;
-            curr = curr.prev;
-        }
-        if(prev != null){
-            llist = prev.prev;
-        }
-        return llist;
+public static DoublyLinkedListNode reverse(DoublyLinkedListNode llist)
+{
+    DoublyLinkedListNode? curr = llist;
+    DoublyLinkedListNode? prev = null;
+    
+    while(curr != null){
+        prev = curr.prev;
+        curr.prev = curr.next;
+        curr.next = prev;
+        curr = curr.prev;
     }
+    if(prev != null){
+        llist = prev.prev;
+    }
+    return llist;
+}
