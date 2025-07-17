b382ddd13d2710d0c3a903c852fdf77cea0aa800
johannacarolinep <johannacarolinep@gmail.com>
2025-07-17 23:25:21 +0200

    chore: test commit

--- string_list_anagrams/solutions.cs
+++ string_list_anagrams/solutions.cs
@@ -1,22 +1,22 @@
 public static int sherlockAndAnagrams(string s)
 {
-    Dictionary<string,int> subStringCounts = new Dictionary<string, int>();
-    for(int i =0; i<s.Length; i++){
-        for(int j = 1; j<=s.Length-i; j++){
-            string substring = s.Substring(i,j);
+    Dictionary<string, int> subStringCounts = new Dictionary<string, int>();
+    for (int i = 0; i < s.Length; i++) {
+        for (int j = 1; j <= s.Length - i; j++) {
+            string substring = s.Substring(i, j);
             string sorted = String.Concat(substring.OrderBy(c => c));
-            if(subStringCounts.ContainsKey(sorted)){
+            if (subStringCounts.ContainsKey(sorted)) {
                 subStringCounts[sorted]++;
-            }else{
+            } else {
                 subStringCoSunts[sorted] = 1;
             }
         }
     }
     int count = 0;
-    foreach(var kvp in subStringCounts){
+    foreach (var kvp in subStringCounts) {
         int freq = kvp.Value;
-        if(freq > 1){
-            count += (freq*(freq-1))/2; 
+        if (freq > 1) {
+            count += (freq * (freq - 1)) / 2;
         }
     }
     return count;
