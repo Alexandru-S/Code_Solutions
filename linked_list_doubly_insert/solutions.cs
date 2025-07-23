3d8e7c14179132bcf7dcea43c2678bf89772a9f1
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-07-23 23:51:25 +0200

    chore: wip doubly linked list

---
+++ linked_list_doubly_insert/solutions.cs
@@ -0,0 +1,8 @@
+    public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data)
+    {
+        DoublyLinkedListNode newdata = new DoublyLinkedListNode(data);
+        while(llist != null && llist.next !=  null){
+            
+        }
+        return newdata;
+    }
