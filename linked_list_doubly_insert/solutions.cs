fd9537264764525cba22cb38794c21c4c10b81b4
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-07-27 18:05:31 +0200

    added doubly linked list solution

--- linked_list_doubly_insert/solutions.cs
+++ linked_list_doubly_insert/solutions.cs
@@ -1,8 +1,23 @@
-    public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data)
-    {
-        DoublyLinkedListNode newdata = new DoublyLinkedListNode(data);
-        while(llist != null && llist.next !=  null){
-            
-        }
+public static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode llist, int data)
+{
+    DoublyLinkedListNode newdata = new DoublyLinkedListNode(data);
+    if(llist == null){
         return newdata;
     }
+    if(data < llist.data){
+        newdata.next = llist;
+        llist.prev = newdata;
+        return newdata;
+    }
+    DoublyLinkedListNode current = llist;
+    while(current.next != null && current.next.data < data){
+        current = current.next;
+    }
+    newdata.next = current.next;
+    newdata.prev = current;
+    if(current.next != null){
+        current.next.prev = newdata;
+    }
+    current.next = newdata;
+    return llist;
+}
