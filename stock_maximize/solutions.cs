c8fd97d359da93a5458183f6f7eaa66285476bc1
Alexandru-S <10687004+Alexandru-S@users.noreply.github.com>
2025-08-06 22:16:32 +0200

    chore: added stock maximize algorythm

--- stock_maximize/solutions.cs
+++ stock_maximize/solutions.cs
@@ -1,4 +1,11 @@
-public static long stockmax(List<int> prices)
-{
-
+public static long stockmax(List<int> prices){
+    long profit = 0;
+    int maxPriceSoFar = 0;
+    for(int i = prices.Count -1; i>=0; i--){
+        if(prices[i] > maxPriceSoFar){
+            maxPriceSoFar = prices[i];
+        }
+        profit += maxPriceSoFar - prices[i];
+    }
+    return profit;
 }
